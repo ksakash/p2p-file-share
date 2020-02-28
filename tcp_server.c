111d53b4c2a8c68e6318447ceff2fc9f1d7a6267
ksakash <akashsingh049811@gmail.com>
2020-02-28 18:27:46 +0530

    added simple tcp client and server

---
+++ tcp_server.c
@@ -0,0 +1,62 @@
+/*
+** tcp_server.c -- a tcp socket server demo
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
+
+#define MYPORT 3490	// the port users will be connecting to
+
+#define BACKLOG 10	 // how many pending connections queue will hold
+
+int main(void)
+{
+	int sockfd, new_fd;  // listen on sock_fd, new connection on new_fd
+	struct sockaddr_in my_addr;	// my address information
+	struct sockaddr_in their_addr; // connector's address information
+	socklen_t sin_size;
+
+	if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) == -1) {
+		perror("socket");
+		exit(1);
+	}
+
+	my_addr.sin_family = AF_INET;		 // host byte order
+	my_addr.sin_port = htons(MYPORT);	 // short, network byte order
+	my_addr.sin_addr.s_addr = INADDR_ANY; // automatically fill with my IP
+	memset(my_addr.sin_zero, '\0', sizeof my_addr.sin_zero);
+
+	if (bind(sockfd, (struct sockaddr *)&my_addr, sizeof my_addr) == -1) {
+		perror("bind");
+		exit(1);
+	}
+
+	if (listen(sockfd, BACKLOG) == -1) {
+		perror("listen");
+		exit(1);
+	}
+
+	while(1) {  // main accept() loop
+		sin_size = sizeof their_addr;
+		if ((new_fd = accept(sockfd, (struct sockaddr *)&their_addr, &sin_size)) == -1) {
+			perror("accept");
+			continue;
+		}
+		printf("tcp_server: got connection from %s\n",inet_ntoa(their_addr.sin_addr));
+		if (send(new_fd, "Hello, world!\n", 14, 0) == -1)
+			perror("send");
+		close(new_fd);
+	}
+
+	return 0;
+}
+
